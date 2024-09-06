109cc0f2d496b33d6dfc4a16cf718cc8f33c26fe
Diogo Baeder <diogobaeder@yahoo.com.br>
2024-09-06 20:54:42 -0300

    Agent able to send traces to Datadog Cloud

--- src/worker.py
+++ src/worker.py
@@ -1,27 +1,10 @@
 import logging
 
-from celery.signals import before_task_publish, task_prerun
 from ddtrace import tracer
-from ddtrace.context import Context
 
 from base import app
 
 logger = logging.getLogger("worker")
-
-
-@before_task_publish.connect
-def propagate_context(sender=None, headers=None, body=None, **kwargs):
-    span = tracer.current_span()
-    headers["trace_id"] = span.trace_id
-    headers["span_id"] = span.span_id
-
-
-@task_prerun.connect
-def hydrate_context(task_id=None, task=None, *args, **kwargs):
-    tracer.context_provider.activate(Context(
-        trace_id=task.request.headers["trace_id"],
-        span_id=task.request.headers["span_id"],
-    ))
 
 
 @app.task()
